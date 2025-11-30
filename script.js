b01132839943e23aabe91fdf41b75d4e3ccc984f
angiecash <142642774+angiecash@users.noreply.github.com>
2025-11-30 17:19:25 -0500

    Updating Footer

--- script.js
+++ script.js
@@ -3,11 +3,12 @@
 // global variables //
 const dropdownCheckbox = document.getElementById('dropdown');
 
-
+//FUNCTIONS//
 function checkMediaQuery() {
     if (window.innerWidth > 991) {
         dropdownCheckbox.checked = false;
     }
 }
 
+// Event Listeners //
 window.addEventListener('resize', checkMediaQuery);
